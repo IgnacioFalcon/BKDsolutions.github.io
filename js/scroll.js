5770edb8978bc174e30d1383861df10db38bbace
Ignacio Falcón <Falconignacio17@gmail.com>
2023-03-03 17:27:50 -0300

    Creo animaciones

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,15 @@
 window.sr = ScrollReveal();
+
+
   sr.reveal('.navbar', {
     duration: 3000,
     origin: 'bottom',
     distance: '-100px'
+  });
+
+
+  sr.reveal('.titulouno', {
+    duration: 3000,
+    origin: 'left',
+    distance: '400px'
   });
